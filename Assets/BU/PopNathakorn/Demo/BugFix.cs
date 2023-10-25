30d51ef06014292157274619e18f83489587d431
Pop Nathakorn <nathakorn.s@notero.co>
2023-10-25 10:46:02 +0700

    2nd time bug fix

--- Assets/BU/PopNathakorn/Demo/BugFix.cs
+++ Assets/BU/PopNathakorn/Demo/BugFix.cs
@@ -5,7 +5,7 @@ public class BugFix : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // Fix bug 2nd time
     }
 
     // Update is called once per frame
